dc435dc41cc1a1d1f03d748463f49e6f5ed0d05b
Annie Huang <annie.hn@gmail.com>
2020-08-09 13:54:41 +1000

    Following the structure of tailwind-default.config.js and extends colors to add primary and second color(with strength)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,15 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#ff6363',
+        secondary: {
+          100: '#e3e3D5',
+          200: '#888883',
+        }
+      }
+    },
   },
   variants: {},
   plugins: [],
